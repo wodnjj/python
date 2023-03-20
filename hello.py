62e6f33700ccabe4df53b40ab6f0d2c36a05a486
wodnjj park <cgk7522@naver.com>
2023-03-20 07:45:47 +0000

    3/20 네이버 영화 크롤링

---
+++ hello.py
@@ -0,0 +1,10 @@
+print('hello world')
+print( '안녕 빅데이터')
+
+# 만약에 1을 누르면 더하기를 하고
+if user == 1:
+    print('더하기')
+# 만약에 2를 누르면 빼기를 해라
+if user == 2:
+    print('빼기')
+    print('이줄은 user 가 2를 입력했을때 동작')
